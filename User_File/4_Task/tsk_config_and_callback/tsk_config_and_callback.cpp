b88a680325ee623b9feaf7c6b07b98c7c910a90f
Changes <625307377@qq.com>
2026-05-11 22:52:26 +0800

    修改文件架构

---
+++ User_File/4_Task/tsk_config_and_callback/tsk_config_and_callback.cpp
@@ -0,0 +1,179 @@
+/**
+ * @file tsk_config_and_callback.cpp
+ * @author 
+ * @brief 临时任务调度测试用函数, 后续用来存放个人定义的回调函数以及若干任务
+ *        Task_Init在freertos.c的MX_FREERTOS_Init中被调用, 用于初始化回调函数等
+ *
+ */
+
+/* Includes ------------------------------------------------------------------*/
+
+#include "tsk_config_and_callback.h"
+
+#include "fdcan.h"
+#include "1_Middleware/Driver/CAN/drv_can.h"
+#include "4_Task/DJI_Motor_task/DJI_Motor_task.h"
+#include "1_Middleware/Driver/WDG/drv_wdg.h"
+#include "1_Middleware/System/Timestamp/sys_timestamp.h"
+
+/* Private macros ------------------------------------------------------------*/
+
+/* Private types -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+// 全局初始化完成标志位
+bool init_finished = false;
+
+/* Private function declarations ---------------------------------------------*/
+
+/* Function prototypes -------------------------------------------------------*/
+
+/**
+ * @brief CAN1回调函数
+ *
+ *
+ */
+void CAN1_Callback(FDCAN_RxHeaderTypeDef &Header, uint8_t *Buffer)
+{
+    switch (Header.Identifier)
+    {
+    case (0x201):
+    {
+        DJI_M3508_CAN_RxCpltCallback(Buffer);
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
+}
+
+/**
+ * @brief 每3600s调用一次
+ *
+ */
+void Task3600s_Callback()
+{
+    SYS_Timestamp.TIM_3600s_PeriodElapsedCallback();
+}
+
+/**
+ * @brief 每1s调用一次
+ *
+ */
+void Task1s_Callback()
+{
+}
+
+/**
+ * @brief 每1ms调用一次
+ *
+ */
+void Task1ms_Callback()
+{
+    // Can定时发送
+    TIM_1ms_CAN_PeriodElapsedCallback();
+    
+    // 喂狗
+    TIM_1ms_IWDG_PeriodElapsedCallback();
+}
+
+/**
+ * @brief 每125us调用一次
+ *
+ */
+void Task125us_Callback()
+{
+}
+
+/**
+ * @brief 每10us调用一次
+ *
+ */
+void Task10us_Callback()
+{
+}
+
+/**
+ * @brief 初始化任务
+ *
+ */
+void Task_Init()
+{
+    SYS_Timestamp.Init(&htim5);
+
+    // 电机的CAN
+    CAN_Init(&hfdcan1, CAN1_Callback);
+
+    // 定时器中断初始化
+    HAL_TIM_Base_Start_IT(&htim4);
+    HAL_TIM_Base_Start_IT(&htim5);
+    HAL_TIM_Base_Start_IT(&htim6);
+    HAL_TIM_Base_Start_IT(&htim7);
+    HAL_TIM_Base_Start_IT(&htim8);
+
+    // 标记初始化完成
+    init_finished = true;
+}
+
+/**
+ * @brief 前台循环任务
+ *
+ */
+void Task_Loop()
+{
+}
+
+/**
+ * @brief GPIO中断回调函数
+ *
+ * @param GPIO_Pin 中断引脚
+ */
+extern "C" void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    if (!init_finished)
+    {
+        return;
+    }
+
+    (void) GPIO_Pin;
+}
+
+/**
+ * @brief 定时器中断回调函数
+ *
+ * @param htim
+ */
+extern "C" void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (!init_finished)
+    {
+        return;
+    }
+
+    // 选择回调函数
+    if (htim->Instance == TIM4)
+    {
+        Task10us_Callback();
+    }
+    else if (htim->Instance == TIM5)
+    {
+        Task3600s_Callback();
+    }
+    else if (htim->Instance == TIM6)
+    {
+        Task1s_Callback();
+    }
+    else if (htim->Instance == TIM7)
+    {
+        Task1ms_Callback();
+    }
+    else if (htim->Instance == TIM8)
+    {
+        Task125us_Callback();
+    }
+}
+
+/************************ COPYRIGHT(C) USTC-ROBOWALKER **************************/
